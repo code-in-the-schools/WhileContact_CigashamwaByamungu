549696067031d6b02e8abd2601b896dd55708378
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-22 17:19:39 +0000

    Everything

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import random
-
-print(random.randint(-100, 100))
+k = random.randint (-100, 100)
 
 found = False
+
+while found == False:
+
+  l = int(input("Enter a number:  "))
+
+  if k==l: 
+    found = True
+    print("You figured it out!")
+  elif k>l:
+    print(str(l), "is more then that")
+  elif k<l:
+    print(str(l), "is less then that")
+  else:
+    print("Sorry try again :) ")
+
